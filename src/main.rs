3695061c863f38e7a9050c91759b888757b97c8e
Dāvis Krēgers <daviskregers@protonmail.com>
2022-03-20 10:08:03 +0200

    read sources.yml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs;
+use serde::Deserialize;
+
+
+extern crate serde_yaml;
+
+#[derive(Deserialize, Debug)]
+struct Source {
+    src: String
+}
+
+#[derive(Deserialize, Debug)]
+struct SourceList {
+    sources: Vec<Source>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file : String = env::current_dir().unwrap().display().to_string() + "/sources.yml";
+    println!("Reading file: {}", file);
+
+    let sources_text : String = fs::read_to_string(file)
+            .expect("Something went wrong when reading the file: {}");
+
+    let sources : SourceList = serde_yaml::from_str(&sources_text)
+                .expect("Something went wrong when parsing sources.yaml");
+
+    for source in &sources.sources {
+        println!("Got source: {:?}", source);
+    }
 }
